a0003d8efc3e6c403a7620fb799747acf709c857
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-01-23 17:50:43 +0530

    used pixbuf over toggling the visbility of the icon

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <X11/XKBlib.h>
+#include <X11/Xlib.h>
 #include <X11/extensions/XKB.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,78 +22,81 @@ void usage()
 
 int main(int argc, char **argv)
 {
-  
   unsigned int status = 0;
   Display *disp = XOpenDisplay(DEFAULT_DISPLAY); // TODO: add option for user to change disps. how would that work?
-
-
+  GdkPixbuf *caps_lock_on, *caps_lock_off, *nums_lock_on, *nums_lock_off; // TODO: make an array for each key?
+  GtkStatusIcon *caps_lock, *nums_lock;
+  
   gtk_init(&argc, &argv);
 
+  caps_lock_on = gdk_pixbuf_new_from_file(ICON_PATH_ON, NULL);
+  caps_lock_off = gdk_pixbuf_new_from_file(ICON_PATH_OFF, NULL);
+  nums_lock_on = gdk_pixbuf_new_from_file(ICON_PATH_ON, NULL);
+  nums_lock_off = gdk_pixbuf_new_from_file(ICON_PATH_OFF, NULL);
   
-  GtkStatusIcon *caps_lock_on = gtk_status_icon_new_from_file(ICON_PATH_ON);
-  GtkStatusIcon *caps_lock_off = gtk_status_icon_new_from_file(ICON_PATH_OFF);
-  GtkStatusIcon *num_lock_on = gtk_status_icon_new_from_file(ICON_PATH_ON);
-  GtkStatusIcon *num_lock_off = gtk_status_icon_new_from_file(ICON_PATH_OFF);
-
-   
-  gtk_status_icon_set_tooltip_text (caps_lock_on, "Caps Lock On");
-  //gtk_status_icon_set_name (status_icon1, "kindpackage");
+  //gtk_status_icon_set_name (status_icon1, "kindpackage"); // what do these do?
   //gtk_status_icon_set_title (status_icon1, "kind");
-  gtk_status_icon_set_tooltip_text (caps_lock_off, "Caps Lock Off");
-  gtk_status_icon_set_tooltip_text (num_lock_on, "Num Lock On");
-  gtk_status_icon_set_tooltip_text (num_lock_off, "Num Lock Off");
 
 
+  // this will make the num lock icon come first!
+  nums_lock = gtk_status_icon_new();
+  gtk_status_icon_set_tooltip_text(nums_lock, "Num Lock");
+  gtk_status_icon_set_visible(nums_lock, True);
 
-  // handle gtk_main_loop withhttps://developer-old.gnome.org/glib/stable/glib-The-Main-Event-Loop.html? register own exits? my main loop func to execute? why this over gtk_main_iterate()?
-  /* what to do now: why not just use gtk_main_iteration_do()? with a False?
-     1. read up on the main loop
-     2. start the loop
-     3. be able to end the loop on my own terms. (also run the kbdindicator
-function)
-     4. stop the loop
-     5. clean up
+  // then this. maybe add patches for this ordering?
+  caps_lock = gtk_status_icon_new();
+  gtk_status_icon_set_tooltip_text (caps_lock, "Caps Lock");
+  gtk_status_icon_set_visible(caps_lock, True);
+  
+  // nothing at all is 0.0%. very nice (as in no icon)
+  // I wonder if, only changing the icon when the status has changed will reduce the load? would that cause a CPU spike? only way to know is to test
+  /*      // don't really like the following code. refactor after figuring out how to display the icons!
+	  // this implementation takes around 0.7% when switching rapidly enough. takes 1.3% at the most, I've tested
+	  if(status != old_status) {
+	if(status & CAPS_LOCK_MASK) {
+	  gtk_status_icon_set_from_pixbuf(caps_lock, caps_lock_on);
+	  fprintf(stderr, "Caps Lock is ON\n");
+	} else {
+	  gtk_status_icon_set_from_pixbuf(caps_lock, caps_lock_off);
+	  fprintf(stderr, "Caps Lock is OFF\n");
+	}
+
+	old_status = status;
+      }
    */
 
+  
+  int old_status = -1; // this makes the loop run at least once
+  
   while(1)
     {
-      gtk_main_iteration();
+      gtk_main_iteration_do(True);
       XkbGetIndicatorState(disp, XkbUseCoreKbd, &status);
-  
+      
       // don't really like the following code. refactor after figuring out how to display the icons!
-      if(status & CAPS_LOCK_MASK) {
-	gtk_status_icon_set_visible (caps_lock_on, TRUE); // any way to make this location dependent? or a way to swap out icon files?
-	gtk_status_icon_set_visible (caps_lock_off, FALSE);
-	fprintf(stderr, "Caps Lock is ON\n");
-      } else {
-	gtk_status_icon_set_visible (caps_lock_off, TRUE);
-	gtk_status_icon_set_visible (caps_lock_on, FALSE);
-	fprintf(stderr, "Caps Lock is OFF\n");
+      if(status != old_status) {
+	if(status & CAPS_LOCK_MASK) {
+	  gtk_status_icon_set_from_pixbuf(caps_lock, caps_lock_on);
+	  fprintf(stderr, "Caps Lock is ON\n");
+	} else {
+	  gtk_status_icon_set_from_pixbuf(caps_lock, caps_lock_off);
+	  fprintf(stderr, "Caps Lock is OFF\n");
+	}
+
+	if(status & NUM_LOCK_MASK) {
+	  gtk_status_icon_set_from_pixbuf(nums_lock, nums_lock_on);
+	  fprintf(stderr, "Num Lock is ON\n");
+	} else {
+	  gtk_status_icon_set_from_pixbuf(nums_lock, nums_lock_off);
+	  fprintf(stderr, "Num Lock is OFF\n");
+	}
+
+	old_status = status;
       }
 
-      if(status & NUM_LOCK_MASK) {
-	gtk_status_icon_set_visible (num_lock_on, TRUE);
-	gtk_status_icon_set_visible (num_lock_off, FALSE);
-	fprintf(stderr, "Num Lock is ON\n");
-      } else {
-	gtk_status_icon_set_visible (num_lock_off, TRUE);
-	gtk_status_icon_set_visible (num_lock_on, FALSE);
-	fprintf(stderr, "Num Lock is OFF\n");
-      }
-      /*
-      if(status & NUM_LOCK_MASK)
-	printf("NumLock is Active\n");
-      else
-	printf("NumLock is Not Active\n");
-
-      if(status & SCRL_LOCK_MASK)
-	printf("Scorll Lock is Active\n");
-      else
-      printf("Scroll Lock is not Active\n");*/
 
     }
-
-
+  
   //TODO: implement cleanup here
   gtk_main_quit();
   return 0;
